6bc5c5ea0af2728bf051eb0095bc467b6f319be7
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2021-10-28 11:28:10 +0100

    added two algorithm files

--- src/main/java/timbulchalka/MinutesToYearsDaysCalculator.java
+++ src/main/java/timbulchalka/MinutesToYearsDaysCalculator.java
@@ -1,4 +1,26 @@
 package timbulchalka;
 
 public class MinutesToYearsDaysCalculator {
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+    public static void main(String[] args) {
+        printYearsAndDays(1051200);
+    }
+
+    public static void printYearsAndDays(long minutes){
+        if(minutes < 0)
+            System.out.println(INVALID_VALUE_MESSAGE);
+
+        else {
+            long years = minutes / 525600L;
+            long days = minutes / 1440;
+            System.out.println(days);
+            long remainingDays = 0;
+            if (days >= 365) {
+                remainingDays = days - 365;
+            }else{
+                remainingDays = days;
+            }
+            System.out.println(minutes + " min " + "= " + years + " y " + "and " + remainingDays + " d");
+        }
+    }
 }
